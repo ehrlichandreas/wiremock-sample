ff331cc27b9db774592cf99d286b69432eb5bf76
Andreas Ehrlich <ehrlich.andreas@googlemail.com>
2019-04-14 19:20:13 +0200

    update to new library

--- src/main/resources/archetype-resources/standalone/src/main/java/Executable.java
+++ src/main/resources/archetype-resources/standalone/src/main/java/Executable.java
@@ -3,29 +3,44 @@ package ${package};
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 
-import com.github.ehrlichandreas.wiremock.starter.WireMockStarter;
+import com.github.ehrlichandreas.wiremock.starter.WireMockProxyStarter;
 
 @Configuration
 @PropertySource(value = "${symbol_dollar}{standalone.properties-path:classpath:standalone.properties}", ignoreResourceNotFound = true)
-public class Executable extends WireMockStarter {
+public class Executable extends WireMockProxyStarter {
     private static String WIREMOCK_SERVER_PORT_PROPERTY_NAME = "wiremock.server.port";
     private static String WIREMOCK_STUBS_DIRECTORY_PROPERTY_NAME = "wiremock.stubs.directory";
     private static String WIREMOCK_STUBS_ROOT_CONTEXT_PROPERTY_NAME = "wiremock.stubs.root-context";
+    private static String WIREMOCK_PROXY_PROPERTIES_PREFIX_NAME = "wiremock.proxy.properties.prefix";
+    private static String WIREMOCK_INIT_PROXIED_SERVERS_PROPERTY_NAME = "wiremock.init.proxied-servers";
 
     public static void main(String[] args) {
-        WireMockStarter wireMockStarter = new Executable();
+        WireMockProxyStarter wireMockStarter = new ExecutableProxy();
         wireMockStarter.startWireMockServer();
     }
 
+    @Override
     public String getWiremockServerPortPropertyName() {
         return WIREMOCK_SERVER_PORT_PROPERTY_NAME;
     }
 
+    @Override
     public String getWiremockStubsDirectoryPropertyName() {
         return WIREMOCK_STUBS_DIRECTORY_PROPERTY_NAME;
     }
 
+    @Override
     public String getWiremockStubsRootContextPropertyName() {
         return WIREMOCK_STUBS_ROOT_CONTEXT_PROPERTY_NAME;
+    }
+
+    @Override
+    public String getWiremockProxyPropertiesPrefix() {
+        return WIREMOCK_PROXY_PROPERTIES_PREFIX_NAME;
+    }
+
+    @Override
+    public String getWiremockInitProxiedServersPropertyName() {
+        return WIREMOCK_INIT_PROXIED_SERVERS_PROPERTY_NAME;
     }
 }
